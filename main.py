a1f9033b54bcb0805e14c33bbcd83883520b7e70
Dima42gok <danulopdima@gmail.com>
2025-12-01 10:41:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter
+from tkinter import *
+
+root = tkinter.Tk()
+root.title("gok42")
+root.geometry("300x250")
+root.iconbitmap("icon.ico")
+
+label1 = Label(text = "Hello world")
+label1.pack()
+
+root.mainloop()
